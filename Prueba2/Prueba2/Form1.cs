524eda03900c5d52c3e05b5512411ffd42c3d4d2
melissavaliente <melissavaliente@MELISSA>
2016-05-14 12:34:00 -0600

    Nada

--- Prueba2/Prueba2/Form1.cs
+++ Prueba2/Prueba2/Form1.cs
@@ -16,5 +16,10 @@ namespace Prueba2
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
